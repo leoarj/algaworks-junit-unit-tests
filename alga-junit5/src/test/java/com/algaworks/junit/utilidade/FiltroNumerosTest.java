9ae49caa5f062ed28a1ca4e3bfb2213f0677f48f
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-11-30 18:01:16 -0400

    feat: Add example of @DisplayNameGeneration

--- alga-junit5/src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ alga-junit5/src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -1,13 +1,13 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+// Anotação para personalizar geraçãop de nome de exibição dos testes.
+// No caso vai substituir underscores definidos no nome
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FiltroNumerosTest {
 
     /**
@@ -16,7 +16,8 @@ class FiltroNumerosTest {
      * lembrando que Assertions.assertIterableEquals também verifica as posições dos elementos.
      * */
     @Test
-    public void deveRetornarNumerosPares() {
+    // Renomeado para testar @DisplayNameGeneration
+    public void deve_retornar_numeros_pares_() {
         List<Integer>  numeros = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
         List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
