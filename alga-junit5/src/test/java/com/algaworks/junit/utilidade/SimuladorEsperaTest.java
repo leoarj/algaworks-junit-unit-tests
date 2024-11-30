7451d3466584fd6cce2ba6c7279942dd98dc8789
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-11-29 20:05:18 -0400

    feat: Add example of conditional disable test

--- alga-junit5/src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ alga-junit5/src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,8 +1,10 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -17,8 +19,16 @@ class SimuladorEsperaTest {
      * org.opentest4j.AssertionFailedError: execution exceeded timeout of 1000 ms by 9001 ms
      * */
     @Test
-    @Disabled("Não é mais aplicável") // exemplo de desativação de um teste
+//    @Disabled("Não é mais aplicável") // exemplo de desativação de um teste
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "ENV") // exemplo de ativação condicional de teste
     void deveEsperarENaoDarTimeout() {
+
+        /*
+        * Ativação condicional de teste.
+        * Aborta o teste caso a suposição seja verdadeira, e executa um Supplier para uma mensagem especificada.
+        * */
+//        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), () -> "Abortando teste: Não deve ser executado em PROD");
+
         Assertions.assertTimeout(Duration.ofSeconds(2),
                 () -> SimuladorEspera.esperar(Duration.ofSeconds(1)));
     }
