ff2b049e3d7b18b171ce7943855bb7e7943eb1f2
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-11-30 17:12:52 -0400

    feat: Add challenge of unit tests
    Add challenge of unit tests for bank account simulation, with withdraw
    and deposit operations.

--- alga-junit5/src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ alga-junit5/src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -1,27 +1,54 @@
 package com.algaworks.junit.utilidade;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class ContaBancaria {
 
+    private BigDecimal saldo = BigDecimal.ZERO;
+
     public ContaBancaria(BigDecimal saldo) {
-        //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
-        //TODO 2 - pode ser zero ou negativo
+        //DONE 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
+        if (Objects.isNull(saldo)) {
+            throw new IllegalArgumentException("Saldo não pode ser nulo");
+        }
+        //DONE 2 - pode ser zero ou negativo
+        this.saldo = this.saldo.add(saldo);
     }
 
     public void saque(BigDecimal valor) {
-        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
-        //TODO 2 - Deve subtrair o valor do saldo
-        //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+        //DONE 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        validarValor(valor);
+
+        //DONE 2 - Deve subtrair o valor do saldo
+        //DONE 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+        if (saldo.compareTo(valor) < 0) {
+            throw new RuntimeException("Saldo insuficiente");
+        }
+
+        saldo = saldo.subtract(valor);
     }
 
     public void deposito(BigDecimal valor) {
-        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        //DONE 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        validarValor(valor);
         //TODO 2 - Deve adicionar o valor ao saldo
+        saldo = saldo.add(valor);
     }
 
     public BigDecimal saldo() {
-        //TODO 1 - retornar saldo
-        return null;
+        //DONE 1 - retornar saldo
+        return this.saldo;
+    }
+
+    private void validarValor(BigDecimal valor) {
+        if (Objects.isNull(valor)) {
+            throw new IllegalArgumentException("Valor não pode ser nulo");
+        }
+
+        switch (valor.compareTo(BigDecimal.ZERO)) {
+            case -1 -> throw new IllegalArgumentException("Valor não pode ser negativo");
+            case 0 -> throw new IllegalArgumentException("Valor não pode ser zero");
+        }
     }
 }
