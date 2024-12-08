7fe226802f0605069ad3451e23d6cab574ff28e2
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-12-08 15:05:48 -0400

    fix: Fix problem with unused mocks

--- alga-junit5/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ alga-junit5/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -3,10 +3,7 @@ package com.algaworks.junit.blog.negocio;
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
 import com.algaworks.junit.blog.exception.RegraNegocioException;
 import com.algaworks.junit.blog.modelo.Editor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -74,136 +71,160 @@ public class CadastroEditorComMockTest {
     @InjectMocks
     CadastroEditor cadastroEditor;
 
-    @BeforeEach
-    void init() {
-        // Define comportamentos dos mocks
+    @Nested
+    class CadastroComEditorValido {
+
+        @BeforeEach
+        void init() {
+            // Define comportamentos dos mocks
 //        Mockito.when(armazenamentoEditor.salvar(editor))
 //                .thenReturn(new Editor(1L, "Leandro", "leandro@email.com", BigDecimal.TEN, true));
 
-        // Com thenAnswer() é possível personalizar o parâmetro passado na invocação.
-        // Com Mockito.any() é possível deixar o parâmetro dinâmico para o tipo definido, em vez de passar um objeto fixo
-        Mockito.when(armazenamentoEditor.salvar(Mockito.any(Editor.class)))
-                .thenAnswer(invocation -> {
-                   Editor editorPassado = invocation.getArgument(0, Editor.class);
-                   editorPassado.setId(1L);
-                   return editorPassado;
-                });
+            // Com thenAnswer() é possível personalizar o parâmetro passado na invocação.
+            // Com Mockito.any() é possível deixar o parâmetro dinâmico para o tipo definido, em vez de passar um objeto fixo
+            // Obs.: Essa definição nunca vai ser usada caso um editor for nulo, por isso está em uma inner-class.
+            Mockito.when(armazenamentoEditor.salvar(Mockito.any(Editor.class)))
+                    .thenAnswer(invocation -> {
+                        Editor editorPassado = invocation.getArgument(0, Editor.class);
+                        editorPassado.setId(1L);
+                        return editorPassado;
+                    });
 
-    }
+        }
 
-    @Test
-    void Dado_um_editor_valido_Quando_criar_Entao_deve_retornar_um_id_de_cadastro() {
-        Editor editorSalvo = cadastroEditor.criar(editor);
-        assertEquals(1L, editorSalvo.getId());
-    }
+        @Test
+        void Dado_um_editor_valido_Quando_criar_Entao_deve_retornar_um_id_de_cadastro() {
+            Editor editorSalvo = cadastroEditor.criar(editor);
+            assertEquals(1L, editorSalvo.getId());
+        }
 
-    /**
-     * Teste com Mockito.verify(), verifica se chamada do método foi chamada n vezes,
-     * e se o argumento do método passado foi o mesmo repassado para o método da classe que está tendo
-     * o comportamento verificado.
-     * */
-    @Test
-    void Dado_um_editor_valido_Quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento() {
-        cadastroEditor.criar(editor);
-        Mockito.verify(armazenamentoEditor, Mockito.times(1))
-                .salvar(Mockito.eq(editor));
-    }
+        /**
+         * Teste com Mockito.verify(), verifica se chamada do método foi chamada n vezes,
+         * e se o argumento do método passado foi o mesmo repassado para o método da classe que está tendo
+         * o comportamento verificado.
+         * */
+        @Test
+        void Dado_um_editor_valido_Quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento() {
+            cadastroEditor.criar(editor);
+            Mockito.verify(armazenamentoEditor, Mockito.times(1))
+                    .salvar(Mockito.eq(editor));
+        }
 
-    /**
-     * Testa o lançamento de exceptions, customizando um mock para que caso determinado método seja chamado,
-     * uma Runtime exception seja lançada.
-     *
-     * Em seguida, verifica as asserções para que:
-     * - A exceção realmente seja lançada.
-     * - Verifica se o método de um mock deve ser chamado nesta determinada situação.
-     *
-     * thenThrow() = Lança uma exceção caso determinado comportamento seja chamado.
-     * never() = Método do mock nunca deve ser chamado, conforme o fluxo definido.
-     * any() = Qualquer argumento passado.
-     *
-     * */
-    @Test
-    void Dado_um_editor_valido_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_deve_enviar_email() {
-        Mockito.when(armazenamentoEditor.salvar(editor)).thenThrow(new RuntimeException());
-        assertAll("Não deve enviar e-mail, quando lançar exception do armazenamento",
-                () -> assertThrows(RuntimeException.class, () -> cadastroEditor.criar(editor)),
-                () -> Mockito.verify(gerenciadorEnvioEmail, Mockito.never()).enviarEmail(Mockito.any())
-        );
-    }
+        /**
+         * Testa o lançamento de exceptions, customizando um mock para que caso determinado método seja chamado,
+         * uma Runtime exception seja lançada.
+         *
+         * Em seguida, verifica as asserções para que:
+         * - A exceção realmente seja lançada.
+         * - Verifica se o método de um mock deve ser chamado nesta determinada situação.
+         *
+         * thenThrow() = Lança uma exceção caso determinado comportamento seja chamado.
+         * never() = Método do mock nunca deve ser chamado, conforme o fluxo definido.
+         * any() = Qualquer argumento passado.
+         *
+         * */
+        @Test
+        void Dado_um_editor_valido_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_deve_enviar_email() {
+            Mockito.when(armazenamentoEditor.salvar(editor)).thenThrow(new RuntimeException());
+            assertAll("Não deve enviar e-mail, quando lançar exception do armazenamento",
+                    () -> assertThrows(RuntimeException.class, () -> cadastroEditor.criar(editor)),
+                    () -> Mockito.verify(gerenciadorEnvioEmail, Mockito.never()).enviarEmail(Mockito.any())
+            );
+        }
 
-    /**
-     * Teste para ArgumentCaptor, para capturar argumentos em uma chamada de método de um mock.
-     * No caso, capturando o argumento que foi passado e recuperando o valor para realizar asserções necessárias.
-     *
-     * Para esta situação, o e-mail do destinatário da mensagem deve ser o mesmo que do editor.
-     * */
-    @Test
-    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_com_destino_ao_editor() {
-        // Criando ArgumentCaptor
+        /**
+         * Teste para ArgumentCaptor, para capturar argumentos em uma chamada de método de um mock.
+         * No caso, capturando o argumento que foi passado e recuperando o valor para realizar asserções necessárias.
+         *
+         * Para esta situação, o e-mail do destinatário da mensagem deve ser o mesmo que do editor.
+         * */
+        @Test
+        void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_com_destino_ao_editor() {
+            // Criando ArgumentCaptor
 //        ArgumentCaptor<Mensagem> mensagemArgumentCaptor = ArgumentCaptor.forClass(Mensagem.class);
 
-        // Internamento vai chamar o envio de e-mail
-        Editor editorSalvo = cadastroEditor.criar(editor);
+            // Internamento vai chamar o envio de e-mail
+            Editor editorSalvo = cadastroEditor.criar(editor);
 
-        // Configura a captura, para quando o método do mock for chamado
-        Mockito.verify(gerenciadorEnvioEmail).enviarEmail(mensagemArgumentCaptor.capture());
+            // Configura a captura, para quando o método do mock for chamado
+            Mockito.verify(gerenciadorEnvioEmail).enviarEmail(mensagemArgumentCaptor.capture());
 
-        // Recupera o valor argumento passado no método do mock
-        Mensagem mensagem = mensagemArgumentCaptor.getValue();
+            // Recupera o valor argumento passado no método do mock
+            Mensagem mensagem = mensagemArgumentCaptor.getValue();
 
-        assertEquals(editorSalvo.getEmail(), mensagem.getDestinatario());
-    }
+            assertEquals(editorSalvo.getEmail(), mensagem.getDestinatario());
+        }
 
-    /**
-     * Teste com spy().
-     * spy() = Permite observar objetos e verificações de chamadas de seus métodos.
-     * O objeto "espionado" deve ser passado como argumento nos métodos que se deseja testar.
-     * */
-    @Test
-    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_verificar_o_email() {
-        // Criando objeto spy
+        /**
+         * Teste com spy().
+         * spy() = Permite observar objetos e verificações de chamadas de seus métodos.
+         * O objeto "espionado" deve ser passado como argumento nos métodos que se deseja testar.
+         * */
+        @Test
+        void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_verificar_o_email() {
+            // Criando objeto spy
 //        Editor editorSpy = Mockito.spy(editor);
-        cadastroEditor.criar(editor);
-        Mockito.verify(editor, Mockito.atLeast(1)).getEmail();
+            cadastroEditor.criar(editor);
+            Mockito.verify(editor, Mockito.atLeast(1)).getEmail();
+        }
+
+        /**
+         * Exemplo de teste com alteração do retorno conforme chamadas consecutivas.
+         * */
+        @Test
+        void Dado_um_editor_com_email_existente_Quando_cadastrar_Entao_deve_lancar_exception() {
+            /*
+             * Configura para quando chamar na primeira vez, retorne um Optional vazio,
+             * na segunda vez um objeto (já cadastrado).
+             *
+             * Dessa forma, na segunda chamada do método que depende da verificação do email,
+             * é esperado que retorne uma exception.
+             * */
+            Mockito.when(armazenamentoEditor.encontrarPorEmail("leandro@email.com"))
+                    .thenReturn(Optional.empty()) // definição para primeira chamada
+                    .thenReturn(Optional.of(editor)); // definição para segunda chamada
+
+            Editor editorComEmailExistente = new Editor(null, "Leandro", "leandro@email.com", BigDecimal.TEN, true);
+
+            cadastroEditor.criar(editor); // execução de primeira chamada
+
+            // execução de segunda chamada
+            assertThrows(RegraNegocioException.class, () -> cadastroEditor.criar(editorComEmailExistente));
+        }
+
+        /**
+         * Exemplo de verificação da ordem de execução de métodos para objetos mock.
+         * Útil para assegurar que uma sequência correta de operações seja chamada na regra de negócio.
+         * */
+        @Test
+        void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_apos_salvar() {
+            cadastroEditor.criar(editor);
+
+            // armazenamentoEditor e gerenciadorEnvioEmail já são mocks
+            // aqui está verificando a sequência de execção deles
+            InOrder inOrder = Mockito.inOrder(armazenamentoEditor, gerenciadorEnvioEmail);
+            inOrder.verify(armazenamentoEditor, Mockito.times(1)).salvar(editor);
+            inOrder.verify(gerenciadorEnvioEmail, Mockito.times(1)).enviarEmail(Mockito.any(Mensagem.class));
+        }
+
     }
 
-    /**
-     * Exemplo de teste com alteração do retorno conforme chamadas consecutivas.
-     * */
-    @Test
-    void Dado_um_editor_com_email_existente_Quando_cadastrar_Entao_deve_lancar_exception() {
-        /*
-        * Configura para quando chamar na primeira vez, retorne um Optional vazio,
-        * na segunda vez um objeto (já cadastrado).
-        *
-        * Dessa forma, na segunda chamada do método que depende da verificação do email,
-        * é esperado que retorne uma exception.
-        * */
-        Mockito.when(armazenamentoEditor.encontrarPorEmail("leandro@email.com"))
-                .thenReturn(Optional.empty()) // definição para primeira chamada
-                .thenReturn(Optional.of(editor)); // definição para segunda chamada
+    @Nested
+    class CadastroComEditorNull {
 
-        Editor editorComEmailExistente = new Editor(null, "Leandro", "leandro@email.com", BigDecimal.TEN, true);
+        /**
+         * Como no teste de editor nulo, as definições de invocação não serão usadas,
+         * o Mockito lança uma exception de mocks não utilizados.
+         * Para resolver isso, pode-se criar inner-classes para separar os testes que dependem
+         * das definições dos que não dependem.
+         * */
+        @Test
+        void Dado_um_editor_null_Quando_cadastrar_Entao_deve_lancar_exception() {
+            assertThrows(NullPointerException.class, () -> cadastroEditor.criar(editor));
+            Mockito.verify(armazenamentoEditor, Mockito.never()).salvar(Mockito.any());
+            Mockito.verify(gerenciadorEnvioEmail, Mockito.never()).enviarEmail(Mockito.any());
+        }
 
-        cadastroEditor.criar(editor); // execução de primeira chamada
-
-        // execução de segunda chamada
-        assertThrows(RegraNegocioException.class, () -> cadastroEditor.criar(editorComEmailExistente));
-    }
-
-    /**
-     * Exemplo de verificação da ordem de execução de métodos para objetos mock.
-     * Útil para assegurar que uma sequência correta de operações seja chamada na regra de negócio.
-     * */
-    @Test
-    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_apos_salvar() {
-        cadastroEditor.criar(editor);
-
-        // armazenamentoEditor e gerenciadorEnvioEmail já são mocks
-        // aqui está verificando a sequência de execção deles
-        InOrder inOrder = Mockito.inOrder(armazenamentoEditor, gerenciadorEnvioEmail);
-        inOrder.verify(armazenamentoEditor, Mockito.times(1)).salvar(editor);
-        inOrder.verify(gerenciadorEnvioEmail, Mockito.times(1)).enviarEmail(Mockito.any(Mensagem.class));
     }
 
 }
